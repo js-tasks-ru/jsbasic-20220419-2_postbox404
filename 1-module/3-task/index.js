75a732e7f6b2ed6f206691f97fd5cef02719c4cc
Dmitry Ivannikov <postbox404@gmail.com>
2022-04-23 10:48:13 +1000

    1-module-3-task

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,3 +1,8 @@
 function ucFirst(str) {
-  // ваш код...
+  let firstUpperLetter = str.charAt(0).toUpperCase();
+	let arrStr = str.split('');
+	arrStr.shift();
+	let strWithOutFirstLetter = arrStr.join('');
+	let rezultStr = firstUpperLetter + strWithOutFirstLetter
+	return rezultStr;
 }
