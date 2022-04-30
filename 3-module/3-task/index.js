1910e5403eec2370f5baa6c3a0c7abe5149668eb
Dmitry Ivannikov <postbox404@gmail.com>
2022-04-30 12:31:34 +1000

    3-module-3-task

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,10 @@
 function camelize(str) {
-  // ваш код...
+  let strToArray = str.split('')
+	for (let i = 0; i < strToArray.length; i++) {
+		if (strToArray.includes('-')) {
+			strToArray.splice(strToArray.indexOf('-'), 2, strToArray[strToArray.indexOf('-') + 1].toUpperCase());
+		}
+	}	
+	let arrToString = strToArray.join('');
+	return arrToString;	
 }
