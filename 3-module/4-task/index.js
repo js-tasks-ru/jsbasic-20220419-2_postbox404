b07f6f995dae785df9591c10561b4dbf09a82b2f
Dmitry Ivannikov <postbox404@gmail.com>
2022-04-30 13:40:45 +1000

    3-module-4-task

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,9 @@
 function showSalary(users, age) {
-  // ваш код...
+  let filterArr = users.filter(el => el['age'] <= age);
+	let namesArr = filterArr.map(person => `${person['name']}, ${person['balance']}`);
+	let str = '';
+	for (let i = 0; i < namesArr.length - 1; i++) {
+		str += `${namesArr[i]}\n`;
+	}
+	return str + namesArr[namesArr.length - 1];
 }
