10811d50c0eac8a98b06012cb7952f0d450a1228
Dmitry Ivannikov <postbox404@gmail.com>
2022-05-07 06:10:19 +1000

    4-module-3-task

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,25 @@
 function highlight(table) {
-  // ваш код...
+	for (let i = 1; i < table.rows.length; i++) {
+		if (!table.rows[i].cells[3].hasAttribute('data-available')) {
+			table.rows[i].setAttribute('hidden', 'hidden');
+		}
+		if (table.rows[i].cells[3].getAttribute('data-available') === 'true') {
+			table.rows[i].classList.add('available');
+		}
+		if (table.rows[i].cells[3].getAttribute('data-available') === 'false') {
+			table.rows[i].classList.add('unavailable');
+		}
+		if (table.rows[i].cells[2].innerHTML === 'm') {
+			table.rows[i].classList.add('male');
+		}
+		if (table.rows[i].cells[2].innerHTML === 'f') {
+			table.rows[i].classList.add('female');
+		}
+		if (+table.rows[i].cells[1].innerHTML < 18) {
+			table.rows[i].style.textDecoration = 'line-through';
+		}
+	}
 }
+
+
+
