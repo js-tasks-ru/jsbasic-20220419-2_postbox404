9959589000b333a630d81c1686ab361aa1c3f22b
Dmitry Ivannikov <postbox404@gmail.com>
2022-05-11 21:25:56 +1000

    5-module-3-task

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,31 @@
 function initCarousel() {
-  // ваш код...
+
+  let buttonRight = document.querySelector('.carousel__arrow_right');
+  let buttonLeft = document.querySelector('.carousel__arrow_left');
+  let carouselInner = document.querySelector('.carousel__inner');
+  let carouselIttems = document.querySelectorAll('.carousel__slide');
+  let carouselIttemsLength = carouselIttems.length;
+  let carouselIttemsWidth = document.querySelector('.carousel__slide').offsetWidth;
+  let procentMoove = 0;
+
+  hiddenButton();
+    
+  buttonRight.addEventListener('click', () => {
+    carouselInner.style.transform = `translateX(${procentMoove -= carouselIttemsWidth}px)`; 
+    hiddenButton();
+  });
+  buttonLeft.addEventListener('click', () => {
+    carouselInner.style.transform = `translateX(${procentMoove += carouselIttemsWidth}px)`;
+    hiddenButton();
+  });
+
+  function hiddenButton() {
+    if (procentMoove <= -(carouselIttemsLength - 1) * carouselIttemsWidth) {
+      buttonRight.style.display = 'none';
+    } else { buttonRight.style.display = '' }
+    if (procentMoove === 0) {
+      buttonLeft.style.display = 'none';
+    } else { buttonLeft.style.display = '' }
+  }  
 }
+
